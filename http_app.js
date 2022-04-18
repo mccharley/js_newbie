3718e8ecf259501a5f9dbcc1eeb3fd708668406d
mccharley <55229490+mccharley@users.noreply.github.com>
2022-04-18 09:48:10 +0100

    updated code feature

--- http_app.js
+++ http_app.js
@@ -1,5 +1,16 @@
 const http = require('http');
-var portnum = 2065; //port number
+
+function PortNumber(){ //select a random port number to listen to
+        var RandNum = Math.floor(Math.random() * 10);
+        if(RandNum > 4){
+            var RandPort = 2065; //port number
+                        }
+        if(RandNum <= 4){
+            var RandPort = 5046; //port number
+                        }
+        return RandPort;
+                    }
+
 
 //creates a server. A server basically has all the features of an emitter.
 //the create server object takes a call back with 2 parameters (request, response)
@@ -13,7 +24,10 @@ const server = http.createServer((req, res) => {
         res.write(JSON.stringify(['BTC', 'ETH', 'ADA']));
         res.end();
     }
+    console.log('Activity detected on port', portnum,'...');
 });
+
+var portnum = PortNumber();//generate port number
 
 //we start the server listening so it pays attention to events on the port and returns feedback to the 
 server.listen(portnum);
